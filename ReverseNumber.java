34bd9c1f9e614f1584a03c619eb1b564ff8407dd
Christopher Joshy <144864811+ChristopherJoshy@users.noreply.github.com>
2024-10-06 10:27:08 +0530

    Add files via upload

---
+++ ReverseNumber.java
@@ -0,0 +1,20 @@
+package PractieceJavalab1;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        int reversed = 0;
+
+        while (number != 0) {
+            int digit = number % 10;
+            reversed = reversed * 10 + digit;
+            number /= 10;
+        }
+
+        System.out.println("Reversed Number: " + reversed);
+    }
+}
